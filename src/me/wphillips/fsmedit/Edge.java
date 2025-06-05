8aa1ad495fde3a9053dbde03eea6061c8a48ecbc
Wyatt Phillips <119454429+wyattphillips-philips@users.noreply.github.com>
2025-06-04 20:55:51 -0400

    Move classes to me.wphillips.fsmedit package

---
+++ src/me/wphillips/fsmedit/Edge.java
@@ -0,0 +1,19 @@
+package me.wphillips.fsmedit;
+
+public class Edge {
+    private Node from;
+    private Node to;
+
+    public Edge(Node from, Node to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Node getFrom() {
+        return from;
+    }
+
+    public Node getTo() {
+        return to;
+    }
+}
---
+++ src/me/wphillips/fsmedit/GraphEditor.java
@@ -0,0 +1,24 @@
+package me.wphillips.fsmedit;
+
+import javax.swing.*;
+
+public class GraphEditor {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("FSM Graph Editor");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            GraphPanel panel = new GraphPanel();
+            Node a = new Node(100, 100, 30, "A");
+            Node b = new Node(250, 100, 30, "B");
+            panel.addNode(a);
+            panel.addNode(b);
+            panel.addEdge(new Edge(a, b));
+
+            frame.add(panel);
+            frame.setSize(400, 300);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+}
