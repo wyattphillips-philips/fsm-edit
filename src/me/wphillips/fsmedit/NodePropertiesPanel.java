f61d428ef42d7f2ee062c3e50854a1f3acde5b76
Wyatt Phillips <119454429+wyattphillips-philips@users.noreply.github.com>
2025-06-19 12:30:16 -0400

    Add position and metadata editing

--- src/me/wphillips/fsmedit/NodePropertiesPanel.java
+++ src/me/wphillips/fsmedit/NodePropertiesPanel.java
@@ -3,6 +3,8 @@ package me.wphillips.fsmedit;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -10,8 +12,15 @@ import java.awt.*;
 public class NodePropertiesPanel extends JPanel {
     private final JLabel labelLabel;
     private final JTextField labelField;
+    private final JLabel xLabel;
+    private final JSpinner xSpinner;
+    private final JLabel yLabel;
+    private final JSpinner ySpinner;
     private final JLabel colorLabel;
     private final JButton colorButton;
+    private final JLabel metadataLabel;
+    private final JTextArea metadataArea;
+    private final JScrollPane metadataScroll;
     private final GraphPanel graphPanel;
     private Node node;
 
@@ -45,6 +54,48 @@ public class NodePropertiesPanel extends JPanel {
         });
         gbc.weightx = 1.0;
         add(labelField, gbc);
+
+        // X position
+        gbc.gridy++;
+        gbc.weightx = 0;
+        xLabel = new JLabel("X:");
+        add(xLabel, gbc);
+        gbc.gridy++;
+        xSpinner = new JSpinner(new SpinnerNumberModel(0, -10000, 10000, 1));
+        xSpinner.setEnabled(false);
+        xSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (node != null) {
+                    node.setX((Integer) xSpinner.getValue());
+                    graphPanel.repaint();
+                }
+            }
+        });
+        gbc.weightx = 1.0;
+        add(xSpinner, gbc);
+
+        // Y position
+        gbc.gridy++;
+        gbc.weightx = 0;
+        yLabel = new JLabel("Y:");
+        add(yLabel, gbc);
+        gbc.gridy++;
+        ySpinner = new JSpinner(new SpinnerNumberModel(0, -10000, 10000, 1));
+        ySpinner.setEnabled(false);
+        ySpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (node != null) {
+                    node.setY((Integer) ySpinner.getValue());
+                    graphPanel.repaint();
+                }
+            }
+        });
+        gbc.weightx = 1.0;
+        add(ySpinner, gbc);
+
+        // Color
         gbc.gridy++;
         gbc.weightx = 0;
         colorLabel = new JLabel("Color:");
@@ -65,8 +116,35 @@ public class NodePropertiesPanel extends JPanel {
         });
         gbc.weightx = 1.0;
         add(colorButton, gbc);
+
+        // Metadata text area
+        gbc.gridy++;
+        gbc.weightx = 0;
+        metadataLabel = new JLabel("Metadata:");
+        add(metadataLabel, gbc);
+        gbc.gridy++;
+        metadataArea = new JTextArea(5, 10);
+        metadataArea.setLineWrap(true);
+        metadataArea.setWrapStyleWord(true);
+        metadataArea.setEnabled(false);
+        metadataArea.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent e) { update(); }
+            public void removeUpdate(DocumentEvent e) { update(); }
+            public void changedUpdate(DocumentEvent e) { update(); }
+            private void update() {
+                if (node != null) {
+                    node.setMetadata(metadataArea.getText());
+                }
+            }
+        });
+        metadataScroll = new JScrollPane(metadataArea);
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        add(metadataScroll, gbc);
+
         gbc.weighty = 1;
         gbc.gridy++;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         add(Box.createVerticalGlue(), gbc);
         setPreferredSize(new Dimension(180, 0));
 
@@ -79,16 +157,31 @@ public class NodePropertiesPanel extends JPanel {
         boolean visible = node != null;
         labelLabel.setVisible(visible);
         labelField.setVisible(visible);
+        xLabel.setVisible(visible);
+        xSpinner.setVisible(visible);
+        yLabel.setVisible(visible);
+        ySpinner.setVisible(visible);
         colorLabel.setVisible(visible);
         colorButton.setVisible(visible);
+        metadataLabel.setVisible(visible);
+        metadataScroll.setVisible(visible);
         labelField.setEnabled(visible);
+        xSpinner.setEnabled(visible);
+        ySpinner.setEnabled(visible);
         colorButton.setEnabled(visible);
+        metadataArea.setEnabled(visible);
         if (node == null) {
             labelField.setText("");
+            xSpinner.setValue(0);
+            ySpinner.setValue(0);
             colorButton.setBackground(null);
+            metadataArea.setText("");
         } else {
             labelField.setText(node.getLabel());
+            xSpinner.setValue(node.getX());
+            ySpinner.setValue(node.getY());
             colorButton.setBackground(node.getColor());
+            metadataArea.setText(node.getMetadata());
         }
         revalidate();
         repaint();
