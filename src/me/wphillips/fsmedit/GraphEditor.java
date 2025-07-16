2eb1301dc91263139949630f9a1153088e7f6b43
Wyatt Phillips <119454429+wyattphillips-philips@users.noreply.github.com>
2025-07-15 21:03:49 -0400

    Start blank project by default

--- src/me/wphillips/fsmedit/GraphEditor.java
+++ src/me/wphillips/fsmedit/GraphEditor.java
@@ -26,17 +26,6 @@ public class GraphEditor {
                             "Failed to open file: " + ex.getMessage(),
                             "Error", JOptionPane.ERROR_MESSAGE);
                 }
-            } else {
-                Node a = new Node(100, 100, 30, "A");
-                Node b = new Node(250, 100, 30, "B");
-                Node c = new Node(175, 200, 30, "C");
-                panel.addNode(a);
-                panel.addNode(b);
-                panel.addNode(c);
-                panel.setStartNode(a);
-                panel.addEdge(new Edge(a, b));
-                panel.addEdge(new Edge(b, c));
-                panel.addEdge(new Edge(c, a));
             }
 
             frame.add(panel, BorderLayout.CENTER);
