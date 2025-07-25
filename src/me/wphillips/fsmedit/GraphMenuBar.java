0f9c4ada89d55cd82cb8bfa83962054f6cc85f14
Wyatt Phillips <119454429+wyattphillips-philips@users.noreply.github.com>
2025-07-25 14:34:10 -0400

    Add accelerator for Snap to Grid

--- src/me/wphillips/fsmedit/GraphMenuBar.java
+++ src/me/wphillips/fsmedit/GraphMenuBar.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
 
 import me.wphillips.fsmedit.GraphIO;
 
@@ -111,6 +112,8 @@ public class GraphMenuBar extends JMenuBar {
 
         JCheckBoxMenuItem snapItem = new JCheckBoxMenuItem("Snap to Grid");
         snapItem.setSelected(panel.isSnapToGrid());
+        snapItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_G, InputEvent.SHIFT_DOWN_MASK));
         snapItem.addActionListener(e -> panel.setSnapToGrid(snapItem.isSelected()));
         viewMenu.add(snapItem);
 
